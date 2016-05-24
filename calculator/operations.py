cf13a4f68560e1bc23b0241719b6395b6a88eec6
Luke <lukepensiero@gmail.com>
2016-05-24 23:22:25 +0000

    init commit, tests passed

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,18 +1,26 @@
 
 def add(*args):
     # your implementation here
-    pass
+    return operate(lambda x, y : x + y, *args)
 
 def subtract(*args):
     # your implementation here
-    pass
+    return operate(lambda x, y: x - y, *args)
 
 def multiply(*args):
     # your implementation here
-    pass
+    return operate(lambda x, y : x * y, *args)
 
 def divide(*args):
     # your implementation here
-    pass
+    return operate(lambda x, y : float(x) / y, *args)
 
 # add your custom operations here
+def operate(operation, *args):
+    if not args:
+        return 0
+    
+    output = args[0]
+    for number in args[1:]:
+        output = operation(output, number)
+    return output
