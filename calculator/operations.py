48fe1846439710b0f388e42a5282c8dd18569908
Jake Fortner <jakefortner1@gmail.com>
2016-05-25 00:28:27 +0000

    finished project

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,18 +1,23 @@
+from functools import reduce
+import sympy
 
 def add(*args):
     # your implementation here
-    pass
+    return sum(args)
 
 def subtract(*args):
     # your implementation here
-    pass
+    return reduce(lambda x,y: x-y, args)
 
 def multiply(*args):
     # your implementation here
-    pass
+    return reduce(lambda x,y: x*y, args)
 
 def divide(*args):
     # your implementation here
-    pass
+    return reduce(lambda x,y: float(x)/float(y), args)
 
 # add your custom operations here
+def plot(*args):
+    expression = sympy.symbols(args[0]) #Turn the 1st argument from text to math xpression
+    sympy.plotting.textplot(expression, args[1], args[2]) #Plot the expression
