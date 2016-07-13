2d06219e53c5c8d5e7b61959ae2f8c81c0c4ddaa
William Raikes <wraikes@hotmail.com>
2016-07-13 00:16:35 +0000

    Finish extensible calculator

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,18 +1,16 @@
+from functools import reduce
+
 
 def add(*args):
-    # your implementation here
-    pass
+    return sum(args)
 
 def subtract(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x - y, args)
 
 def multiply(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x * y, args)
 
 def divide(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x / y, args)
 
 # add your custom operations here
