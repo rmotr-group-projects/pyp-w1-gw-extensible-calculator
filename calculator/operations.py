ff31d527993d7235949c6404c6d592ee0ccb4bb8
William Raikes <wraikes@hotmail.com>
2016-07-13 00:26:47 +0000

    Fix division for Python 2.7

--- calculator/operations.py
+++ calculator/operations.py
@@ -11,6 +11,6 @@ def multiply(*args):
     return reduce(lambda x, y: x * y, args)
 
 def divide(*args):
-    return reduce(lambda x, y: x / y, args)
+    return reduce(lambda x, y: float(x) / y, args)
 
 # add your custom operations here
