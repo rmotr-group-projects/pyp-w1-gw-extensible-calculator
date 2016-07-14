81f3d25ad2d026d67ee62f17ae9a3f91c4593d5b
Brian Wu <poenbwu@gmail.com>
2016-07-14 23:30:40 +0000

    User Defined exceptions

--- calculator/exceptions.py
+++ calculator/exceptions.py
@@ -1,7 +1,8 @@
-
 class InvalidOperation(Exception):
-    pass
+    def __init__(self, operation):
+        self.error_msg = 'InvalidOperation: ' + str(invalid_operation) + 'operation not supported.'
 
 
 class InvalidParams(Exception):
-    pass
+    def __init__(self):
+        self.error_msg = 'InvalidParams:  Given params are invalid.'
