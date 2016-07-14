145e7fb264a2c859d089aae90a99b16e6517519f
Brian Wu <poenbwu@gmail.com>
2016-07-14 23:48:45 +0000

    no calc exception added

--- calculator/exceptions.py
+++ calculator/exceptions.py
@@ -2,7 +2,12 @@ class InvalidOperation(Exception):
     def __init__(self, operation):
         self.error_msg = 'InvalidOperation: ' + str(invalid_operation) + 'operation not supported.'
 
-
 class InvalidParams(Exception):
     def __init__(self):
-        self.error_msg = 'InvalidParams:  Given params are invalid.'
+        self.error_msg = 'InvalidParams: Given params are invalid.'
+
+class NoCalculator(Exception):
+    """docstring for NoCalculator"""
+    def __init__(self):
+        self.error_msg = "NoCalculator: Calculator does not exist"
+        
