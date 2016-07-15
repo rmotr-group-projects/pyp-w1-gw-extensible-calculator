fa917ff4d920d1c91ce300e35d7470c4c6ac4971
taqpol <njosephchen@gmail.com>
2016-07-15 00:04:49 -0700

    changed exponentiation function
    function is now a single lambda

--- calculator/operations.py
+++ calculator/operations.py
@@ -20,15 +20,4 @@ def divide(*args):
 
 # add your custom operations here
 def exponent(*args):
-    '''Returns the first arg raised to the exponent of the 2nd arg, raised to the 3rd arg, etc..)'''
-    if len(args) < 2:
-        return args
-    else:
-        count = 0
-        for arg in args:
-            if count == 0:
-                value = arg
-            else:
-                value = value ** arg
-            count += 1
-    return value
+    return reduce(lambda x, y: x ** y, args)
