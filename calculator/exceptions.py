a2b784961dd2b12b2bac902e1e001579ad31cb6c
Brian Wu <poenbwu@gmail.com>
2016-07-15 00:33:44 +0000

    new exceptions

--- calculator/exceptions.py
+++ calculator/exceptions.py
@@ -9,5 +9,5 @@ class InvalidParams(Exception):
 class NoCalculator(Exception):
     """docstring for NoCalculator"""
     def __init__(self):
-        self.error_msg = "NoCalculator: Calculator does not exist"
-        
+        self.invalid_calc = "NoCalculator: Calculator does not exist"
+        self.invalid_param = 'InvalidParams: Calculator params are invalid.'
