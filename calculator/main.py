519b02070616fad41af2d309a3bf37d147ad4a70
Nicholas Chen <njosephchen@gmail.com>
2016-07-15 06:46:13 +0000

    added math operations and calculator functions

--- calculator/main.py
+++ calculator/main.py
@@ -1,75 +1,63 @@
+from __future__ import print_function
 from datetime import datetime
 
 from calculator.operations import *
 from calculator.exceptions import *
 
+def create_new_calculator(operations={}):
+    
+    new_calc = {
+        'operations' : {},
+        'history' : []
+    }
+    
+    new_calc['operations'] = operations 
+    return new_calc
 
-def create_new_calculator(operations=None):
-    """
-    Creates a configuration dict for a new calculator. Optionally pre loads an
-    initial set of operations. By default a calculator with no operations
-    is created.
+def perform_operation(calc, operation, params):#
+    for num in params:
+        if not isinstance(num, (int)) and not isinstance(num, (float)):
+            raise InvalidParams('Given params are invalid.')
 
-    :param operations: Dict with initial operations.
-                       ie: {'sum': sum_function, ...}
-    """
-    pass
+    for operators in calc['operations'].keys():
+        if operation == operators:
+            break
+    else:
+        raise InvalidOperation('Given operation is invalid.')
 
 
-def perform_operation(calc, operation, params):
-    """
-    Executes given operation with given params. It returns the result of the
-    operation execution.
-
-    :param calc: A calculator.
-    :param operation: String with the operation name. ie: 'add'
-    :param params: Tuple containing the list of nums to operate with.
-                   ie: (1, 2, 3, 4.5, -2)
-    """
-    pass
+    time_of_exec = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    result = calc["operations"][operation](*params)
+    history_record = (time_of_exec, operation, params, result)
+    calc["history"].append(history_record)
+    
+    return result
 
 
 def add_new_operation(calc, operation):
-    """
-    Adds given operation to the list of supported operations for given calculator.
-
-    :param calc: A calculator.
-    :param operation: Dict with the single operation to be added.
-                      ie: {'add': add_function}
-    """
-    pass
+    if not isinstance(operation, (dict)):
+        raise InvalidOperation('Given operation is invalid.')
+    else:
+        operation_name = ''.join([operation_name for operation_name in operation.keys()])
+        calc['operations'][operation_name] = operation[operation_name]
 
 
 def get_operations(calc):
-    """
-    Returns the list of operation names supported by given calculator.
-    """
-    pass
+    return [operation for operation in calc['operations'].keys()]
 
 
 def get_history(calc):
-    """
-    Returns the history of the executed operations since the last reset or
-    since the calculator creation.
-
-    History items must have the following format:
-        (:execution_time, :operation_name, :params, :result)
-
-        ie:
-        ('2016-05-20 12:00:00', 'add', (1, 2), 3),
-    """
-    pass
+    return calc['history']
 
 
 def reset_history(calc):
-    """
-    Resets the calculator history back to an empty list.
-    """
-    pass
+    calc['history'] = []
 
 
 def repeat_last_operation(calc):
-    """
-    Returns the result of the last operation executed in the history.
-    """
-    pass
+    if calc['history'] == []:
+        return None
+    else:
+        last_action = calc['history'][-1]
+        return last_action[3]
+
--- calculator/operations.py
+++ calculator/operations.py
@@ -1,18 +1,34 @@
+from functools import reduce
+
 
 def add(*args):
-    # your implementation here
-    pass
+    return sum(args)
+
 
 def subtract(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x - y, args)
+
 
 def multiply(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: x * y, args)
+
 
 def divide(*args):
-    # your implementation here
-    pass
+    return reduce(lambda x, y: float(x) / float(y), args)
+
+
 
 # add your custom operations here
+def exponent(*args):
+    '''Returns the first arg raised to the exponent of the 2nd arg, raised to the 3rd arg, etc..)'''
+    if len(args) < 2:
+        return args
+    else:
+        count = 0
+        for arg in args:
+            if count == 0:
+                value = arg
+            else:
+                value = value ** arg
+            count += 1
+    return value
