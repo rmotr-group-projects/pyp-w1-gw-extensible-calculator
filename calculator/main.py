f1358c2101ecf2edaf5c0d184a15c5f1a85bd45f
Kenneth Kang <kjkang11@gmail.com>
2016-07-15 16:34:38 +0000

    using lambda to make operations concise. implementing plot operation.

--- calculator/main.py
+++ calculator/main.py
@@ -1,25 +1,17 @@
 from datetime import datetime
 
+
 from calculator.operations import *
 from calculator.exceptions import *
 
 
-def create_new_calculator(operations=None):
-    calc = {'operations' : {}, 'history' : []}
-    if operations:
-        calc['operations'] = operations
-    """
-    Creates a configuration dict for a new calculator. Optionally pre loads an
-    initial set of operations. By default a calculator with no operations
-    is created.
-
-    :param operations: Dict with initial operations.
-                       ie: {'sum': sum_function, ...}
-    """
-    return calc
+def create_new_calculator(operations={}):
+    # returns a new calculator with an optional set of operations
+    return {'operations' : operations, 'history' : []}
 
 
 def perform_operation(calc, operation, params):
+    # performs an operation if possible
     if operation not in calc['operations']:
         raise InvalidOperation("Given operation is invalid.")
     try:
@@ -30,31 +22,14 @@ def perform_operation(calc, operation, params):
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     calc['history'].append((now,operation,params,result))
     return result
-    """
-    Executes given operation with given params. It returns the result of the
-    operation execution.
-
-    :param calc: A calculator.
-    :param operation: String with the operation name. ie: 'add'
-    :param params: Tuple containing the list of nums to operate with.
-                   ie: (1, 2, 3, 4.5, -2)
-    """
-    
 
 
 def add_new_operation(calc, operation):
+    # adds a new operation to the calculator if possible
     try:
-        calc['operations'].update(operation) # appends the new operation to the current ones
+        calc['operations'].update(operation)
     except:
         raise InvalidOperation("Given operation is invalid.")
-    """
-    Adds given operation to the list of supported operations for given calculator.
-
-    :param calc: A calculator.
-    :param operation: Dict with the single operation to be added.
-                      ie: {'add': add_function}
-    """
-    pass
 
 
 def get_operations(calc):
@@ -62,30 +37,17 @@ def get_operations(calc):
         
 
 def get_history(calc):
-    """
-    Returns the history of the executed operations since the last reset or
-    since the calculator creation.
-
-    History items must have the following format:
-        (:execution_time, :operation_name, :params, :result)
-
-        ie:
-        ('2016-05-20 12:00:00', 'add', (1, 2), 3)
-    """
     return calc['history']
 
 
 def reset_history(calc):
-    """
-    Resets the calculator history back to an empty list.
-    """
     calc['history'] = []
 
 
 def repeat_last_operation(calc):
-    """
-    Returns the result of the last operation executed in the history.
-    """
-    if not calc['history']:
+    # returns the last calculated result if possible, otherwise returns None
+    if calc['history']:
+        return calc['history'][-1][-1]
+    else:
         return None
-    return calc['history'][-1][3]
+    
--- calculator/operations.py
+++ calculator/operations.py
@@ -1,23 +1,23 @@
+from functools import reduce
+from sympy import symbols, plotting
+from sympy.parsing.sympy_parser import parse_expr
+
 
 def add(*args):
     return sum(args)
 
 def subtract(*args):
-    if len(args) == 1:
-        return args[0]
-    else:
-        return args[0] - sum(args[1:])
+    return reduce(lambda x,y: x-y, args)
 
 def multiply(*args):
-    result = 1
-    for num in args:
-        result *= num
-    return result
+    return reduce(lambda x,y: x*y, args)
 
 def divide(*args):
-    result = float(args[0])
-    for num in args[1:]:
-        result /= num
-    return result
-#   return (args[0]) / multiply(args[1:])
-# add your custom operations here
+    return reduce(lambda x,y: float(x)/y, args)
+
+def plot(*args):
+    # plot requires 3 arguments: the expression and the min/max range
+    # plot parses the expression and plots it within the specified range
+    expr, min_range, max_range = args
+    x = symbols('x')
+    return plotting.plot(parse_expr(expr),(x,min_range,max_range))
