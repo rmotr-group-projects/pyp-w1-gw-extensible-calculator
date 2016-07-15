09b44baa6388b26bb1961076841947c73c8b5f90
Brian Wu <poenbwu@gmail.com>
2016-07-15 03:43:15 +0000

    changes in reduce function

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,16 +1,29 @@
 def add(*args):
-    return reduce(lambda x, y: x + y, args)    
+    return sum(args[:])   
 
 def subtract(*args):
-    return reduce(lambda x, y: x - y, args)
+    return args[0] - sum(args[1:])
    
 def multiply(*args):
-    return reduce(lambda x, y: x * y, args)
+    value = args[0]
+    for item in args[1:]:
+        value *= item
+    return value
+    
 
 def divide(*args):
-    if reduce(lambda x, y: x % y, args) == 0:
-        return reduce(lambda x, y: x / y, args)
-    else:
-        return(reduce(lambda x, y: x / float(y), args))
+    int_value = args[0]
+    float_value = args[0]
+    for item in args[1:]:
+        int_value /= item
+        float_value /= item
+    if int_value != float_value:
+        return float_value
+    return int_value
+        
+    #if reduce(lambda x, y: x % y, args) == 0:
+      #  return reduce(lambda x, y: x / y, args)
+    #else:
+     #   return(reduce(lambda x, y: x / float(y), args))
    
 # add your custom operations here
