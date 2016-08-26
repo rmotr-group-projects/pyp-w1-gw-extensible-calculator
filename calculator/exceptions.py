83dd2ca6b3e43c7bafa4d9705fcc8b2f7ec4e9c9
Mark <marklechmanik@gmail.com>
2016-08-26 02:32:03 +0000

    Fixed for Python 3

--- calculator/exceptions.py
+++ calculator/exceptions.py
@@ -1,7 +1,7 @@
 
 class InvalidOperation(Exception):
-    pass
+   """Raises an error when an invalid operation is passed to a calculator """
 
 
 class InvalidParams(Exception):
-    pass
+    """Raises an error when an invalid parameter passed through a function of a calculator """
--- calculator/operations.py
+++ calculator/operations.py
@@ -1,20 +1,29 @@
 
 def add(*args):
-    # your implementation here
-    pass
+    x = float(sum(args))
+    return x
 
 def subtract(*args):
     # your implementation here
-    pass
-
+    x = float(args[0])
+    for arg in args[1:]:
+        x = x-float(arg)
+    return x
+    
 def multiply(*args):
     # your implementation here
-    pass
+    x = float(args[0])
+    for arg in args[1:]:
+        x = x*float(arg)
+    return x
 
 def divide(*args):
     # your implementation here
-    pass
-
+    x = float(args[0])
+    for arg in args[1:]:
+        x = x/float(arg)
+    return x
+    
 def plot(*args):
     # OPTIONAL EXTRA CREDIT FUNCTION! 
     # See README for info.
