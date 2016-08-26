6e5d40653264903605fa09bcc92c470236d26819
Charlie Phan <charliegphan@gmail.com>
2016-08-26 19:26:57 +0000

    Extensible Calc for Python 3 to include the '.'

---
+++ calculator/.~c9_invoke_tlOTBV.py
@@ -0,0 +1,43 @@
+from __future__ import division
+
+def add(*args):
+        if len(args) == 1:
+            print(args[0])
+            return args[0]
+        print (sum(args))
+        return sum(args)
+
+def subtract(*args):
+        if len(args) == 1:
+            print(args[0])
+        print (args[0] - sum(args[1::]))
+        return args[0] - sum(args[1::])
+    
+def multiply(*args):
+    multiplication = 1
+    for item in args:
+        multiplication = multiplication * item
+        print (multiplication)
+    return multiplication
+        
+def divide(*args):
+    if len(args)==1:
+        return args[0]
+    division=args[0]    
+    for item in args[1::]:
+    try:
+    print (division)
+    return division
+
+def plot(*args):
+    # OPTIONAL EXTRA CREDIT FUNCTION! 
+    # See README for info.
+    pass
+
+'''   
+try:
+except InvalidOperation:
+    raise InvalidOperation('Given operation is invalid.')
+except InvalidParams:
+    raise InvalidParams('Given params are invalid.')
+'''
