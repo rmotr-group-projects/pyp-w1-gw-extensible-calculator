d2a8f72b65ab91ec2004a6cfcda7a415ccd0d7fd
Josh Kopp <jkopp5549@gmail.com>
2017-01-15 17:04:05 +0000

    solution

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,23 +1,19 @@
+import functools
+
 
 def add(*args):
-    # your implementation here
-    pass
+    return sum(args)
 
 def subtract(*args):
-    # your implementation here
-    pass
+    return functools.reduce(lambda x, y: x-y, args)
 
 def multiply(*args):
-    # your implementation here
-    pass
+    return functools.reduce(lambda x, y: x*y, args)
+
 
 def divide(*args):
-    # your implementation here
-    pass
+    return float(functools.reduce(lambda x, y: x/float(y), args))
+
 
 def plot(*args):
-    # OPTIONAL EXTRA CREDIT FUNCTION! 
-    # See README for info.
     pass
-
-# add your custom operations here
