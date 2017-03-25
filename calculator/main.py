5e18f4dc8bedb0098415aeb840dff22384e9ce7a
Francine Biazin do Nascimento <francine.biazin@gmail.com>
2017-03-25 17:18:27 +0000

    Second commit

--- calculator/main.py
+++ calculator/main.py
@@ -82,7 +82,7 @@ def get_operations(calc):
     """
     Returns the list of operation names supported by given calculator.
     """
-    return calc["operations"].keys()
+    return list(calc["operations"].keys())
 
 
 def get_history(calc):
